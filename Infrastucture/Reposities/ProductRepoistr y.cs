8b851d2fe0299982c605f5fe66703ba32e2112ae
Hatim Rajab <altorbed7@gmail.com>
2026-01-19 07:55:14 +0200

    Update GetAllProducts to use AsNoTracking for performance
    Switched GetAllProducts to use AsNoTracking for improved read-only query performance. Added explanatory comments about tracking and SaveChanges vs SaveChangesAsync usage.

--- Infrastucture/Reposities/ProductRepoistr y.cs
+++ Infrastucture/Reposities/ProductRepoistr y.cs
@@ -27,11 +27,19 @@ namespace Infrastucture.Reposities
             _context.SaveChanges();
         }
 
-        public List<Product> GetAllProducts() => _dbSet.ToList();
-        
+        public List<Product> GetAllProducts() => _context.products.AsNoTracking().ToList();
+
+        // AsNoTracking() improves performance when entities are only read and not updated.
+        // Queries = AsNoTracking() is particularly useful in read-only scenarios, such as displaying data in a web application,
+        // Commands = Tracking() is essential for scenarios where data modifications are required.
+
         public void SaveChanges()
         {
             _context.SaveChanges();
+            // Dif Between SaveChanges and SaveChangesAsync
+            // SaveChanges is a synchronous method that blocks the calling
+            // thread until the operation is complete, while SaveChangesAsync is asynchronous
+            // and allows the calling thread to continue executing other code,recommended for web applications.
         }
 
     }
